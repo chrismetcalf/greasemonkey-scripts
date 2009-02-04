01be6a7b6e81a10dd7685cbe213c519fca75e5e3
Chris Metcalf <krezel@carfrax.lan>
2009-02-03 23:18:28 -0800

    Just a little code cleanup I should have done earlier

--- torrent2azureus.user.js
+++ torrent2azureus.user.js
@@ -4,26 +4,22 @@
 // @include        http://thepiratebay.org/*
 // ==/UserScript==
 
-	var azureus_web_root = "http://magrathea.lan/torrents/";
+var azureus_web_root = "http://magrathea.lan/torrents/";
 
-  var all_links = document.getElementsByTagName('a');
+var all_links = document.getElementsByTagName('a');
 
-	for(var i = 0; i < all_links.length; i++) {
-		link = all_links[i];
-		
-		// Check to see if it's a torrent link
-		if(link.href.match(/^http:\/\/.*\.torrent$/)) {
-			//GM_log("Found torrent link: " + link);
-			
-			// Create a new link target for the link
-			var link_target = azureus_web_root + "index.tmpl?d=u&upurl=" + encodeURI(link.href);
-			
-			// Replace the link target
-			link.href = link_target;
-			link.target = "_blank";
-		}
-		
-		
-	}
-	
-	//GM_log("Done");
+for(var i = 0; i < all_links.length; i++) {
+  link = all_links[i];
+
+  // Check to see if it's a torrent link
+  if(link.href.match(/^http:\/\/.*\.torrent$/)) {
+    //GM_log("Found torrent link: " + link);
+
+    // Create a new link target for the link
+    var link_target = azureus_web_root + "index.tmpl?d=u&upurl=" + encodeURI(link.href);
+
+    // Replace the link target
+    link.href = link_target;
+    link.target = "_blank";
+  }
+}
